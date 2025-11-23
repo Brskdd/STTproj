1d6eeda33a13bfa4a4ae69a827d59cff6be91492
gmwolak <gmwolak@gmail.com>
2025-11-23 18:16:43 -0500

    Update SpeedDatingQuestionsAndCleanedData.R

--- workData/SpeedDatingQuestionsAndCleanedData.R
+++ workData/SpeedDatingQuestionsAndCleanedData.R
@@ -12,12 +12,15 @@ speedDatingCleanedQ2 <- na.omit(SpeedDating)
 
 ## Q2. Which score seems to be the most important for women to want a second date?
 ## This is of interest as there are lots of categories so which category is the most important to a woman wanting another date.
-speedDatingCleanedQ3 <- na.omit(SpeedDating)
+speedDatingCleanedQ2 <- na.omit(SpeedDating)
 
 ## Q3. Do larger age gaps affect if either a man or a woman view each other as attractive?
 ## There are a lot of different age gaps in the data so we are curious if a certain age gap would correlate with either gender viewing the other as less attractive.
-speedDatingCleanedQ4 <- SpeedDating %>%
+speedDatingCleanedQ3 <- SpeedDating %>%
   filter(!is.na(AgeM)) %>%
   filter(!is.na(AgeF))
 
-##Q4. 
+##Q4. Do people find people they view as attractive to be more fun.
+## We are curious about this as we want to know if one person finds another person attractive if that will influence their perspective on how fun the other person is to be around.
+speedDatingCleanedQ4 <- SpeedDating %>%
+  select(AttractiveM, AttractiveF, FunM, FunF)
