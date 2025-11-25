d38d24696c361c1c8f03138e7af50ccad0705f7b
gmwolak <gmwolak@gmail.com>
2025-11-25 11:29:40 -0500

    fully cleaned data

--- workData/SpeedDatingQuestionsAndCleanedData.R
+++ workData/SpeedDatingQuestionsAndCleanedData.R
@@ -4,21 +4,27 @@ library(dplyr)
 ## This is of interest because we are interested to see which gender is more likely to go on another date. 
 speedDatingCleanedQ1 <- SpeedDating %>%
   filter(!is.na(DecisionM)) %>%
-  filter(!is.na(DecisionF))
+  filter(!is.na(DecisionF)) %>%
+  select(DecisionM, DecisionF)
 
 ## Q2. Which score seems to be the most important for men to want a second date?
 ## This is of interest as there are lots of categories so which category is the most important to a man wanting another date.
 speedDatingCleanedQ2 <- na.omit(SpeedDating)
+speedDatingCleanedQ2M <- speedDatingCleanedQ2 %>%
+  select(DecisionM, LikeM, PartnerYesM, AgeM, RaceM, AttractiveM, SincereM, IntelligentM, FunM, AmbitiousM, SharedInterestsM)
 
 ## Q2. Which score seems to be the most important for women to want a second date?
 ## This is of interest as there are lots of categories so which category is the most important to a woman wanting another date.
 speedDatingCleanedQ2 <- na.omit(SpeedDating)
+speedDatingCleanedQ2F <- speedDatingCleanedQ2 %>%
+  select(DecisionF, LikeF, PartnerYesF, AgeF, RaceF, AttractiveF, SincereF, IntelligentF, FunF, AmbitiousF, SharedInterestsF)
 
 ## Q3. Do larger age gaps affect if either a man or a woman view each other as attractive?
 ## There are a lot of different age gaps in the data so we are curious if a certain age gap would correlate with either gender viewing the other as less attractive.
 speedDatingCleanedQ3 <- SpeedDating %>%
   filter(!is.na(AgeM)) %>%
-  filter(!is.na(AgeF))
+  filter(!is.na(AgeF)) %>%
+  select(AgeM, AgeF, AttractiveM, AttractiveF)
 
 ##Q4. Do people find people they view as attractive to be more fun.
 ## We are curious about this as we want to know if one person finds another person attractive if that will influence their perspective on how fun the other person is to be around.
